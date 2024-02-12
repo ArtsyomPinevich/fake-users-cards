c7afda443269497f76a1038f4723a412cfc992c8
Artsyom <artem2000z.pinevitch@yandex.by>
2024-02-12 05:23:40 +0300

    fixed formating

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header/Header';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store/usersSlice/usersSlice';
 
 const HomePage = () => {
@@ -9,7 +9,8 @@ const HomePage = () => {
 
     useEffect(() => {
         dispatch(fetchUsers());
-        console.log('DATA FETCHED PLEASE WATCH OUT HOW MANY API CALLS YOU MAKE ');
+        //to do. Delete this message before deploy
+        console.log('DATA FETCHED PLEASE WATCH OUT HOW MANY API CALLS YOU MAKE (This message is for me) ');
     }, [dispatch]);
 
     return (
