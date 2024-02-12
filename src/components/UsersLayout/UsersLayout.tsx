284f5c2484c234e4564833c7c94a69dfe3738198
Artsyom <artem2000z.pinevitch@yandex.by>
2024-02-12 05:25:16 +0300

    cards are bow correctly displayed using grid

--- src/components/UsersLayout/UsersLayout.tsx
+++ src/components/UsersLayout/UsersLayout.tsx
@@ -1,22 +1,31 @@
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import {useState} from 'react';
+import {useSelector} from 'react-redux';
+import UserCard from '../UserCard/UserCard';
+import './userLayout.css';
 
 const UsersLayout = () => {
-    const users = useSelector((state) => state.users.userList.users);
-
+    const {userList, loading} = useSelector((state) => state.users);
     const [userLimit, setUserLimit] = useState(10);
-    const limitedUsers = users.filter((_, i) => i < userLimit);
-    console.log('must be filtred', limitedUsers);
+
+    const limitedUsers = userList.filter((_, i) => i < userLimit);
     return (
         <section className="user-cards-section">
             <div className="cards-layout">
-                {limitedUsers.map((user) => (
-                    <div className="user-card">
-                        <h2>{user.firstName}</h2>
-                    </div>
-                ))}
+                {loading ? (
+                    <p>loading</p>
+                ) : (
+                    limitedUsers.map((user) => (
+                        <div className="user-card">
+                            <UserCard user={user} />
+                        </div>
+                    ))
+                )}
 
-                <button onClick={() => setUserLimit(userLimit + 5)}>+5 users</button>
+                {userList.length > limitedUsers.length ? (
+                    <button onClick={() => setUserLimit(userLimit + 5)}>+5 users</button>
+                ) : (
+                    <p>there is no more users to show</p>
+                )}
             </div>
         </section>
     );
