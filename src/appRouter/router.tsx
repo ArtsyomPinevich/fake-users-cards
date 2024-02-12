c7d0154c604911cb749d4d623b8c7d1a44ce1ec9
Artsyom <artem2000z.pinevitch@yandex.by>
2024-02-12 05:20:50 +0300

    added dynamic route for userPage component

--- src/appRouter/router.tsx
+++ src/appRouter/router.tsx
@@ -1,17 +1,21 @@
-import { createBrowserRouter } from 'react-router-dom';
+import {createBrowserRouter} from 'react-router-dom';
 import HomePage from '../components/HomePage';
 import About from '../components/About/About';
 import UsersLayout from '../components/UsersLayout/UsersLayout';
 import Favorite from '../components/Favorite/Favorite';
+import ErrorScreen from '../components/ErrorScreen/ErrorScreen';
+import UserPage from '../components/UserPage/UserPage';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <HomePage />,
+        errorElement: <ErrorScreen />,
         children: [
-            { path: '/', index: true, element: <UsersLayout /> },
-            { path: '/favorite', element: <Favorite /> },
-            { path: '/about', element: <About /> },
+            {path: '/', index: true, element: <UsersLayout />},
+            {path: '/:id', element: <UserPage />},
+            {path: '/favorite', element: <Favorite />},
+            {path: '/about', element: <About />},
         ],
     },
 ]);
