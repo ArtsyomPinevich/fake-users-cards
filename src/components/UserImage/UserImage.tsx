30c1faea2187328c47525df3ae4f70c07c6275cf
Artsyom <artem2000z.pinevitch@yandex.by>
2024-02-15 03:54:33 +0300

    user image and like button as component

---
+++ src/components/UserImage/UserImage.tsx
@@ -0,0 +1,31 @@
+import {MdFavorite} from 'react-icons/md';
+import {useDispatch, useSelector} from 'react-redux';
+import {toggleFavorite} from '../../store/usersSlice/usersSlice';
+
+const UserImage = ({image, firstName, lastName, id}) => {
+    const dispatch = useDispatch();
+
+    const handleAddToFavorite = () => {
+        dispatch(toggleFavorite(Number(id)));
+    };
+
+    const isFavorite = useSelector((state) => state.users.isFavorite[id]);
+
+    return (
+        <div className="user-preview-card__img-wrapper">
+            <img src={image} alt={`${firstName} ${lastName} picture`} loading="lazy" width="300px" height="300px" />
+
+            <button className="user-preview-card__add-to-favorite" onClick={handleAddToFavorite} title="like button">
+                <MdFavorite
+                    size="48px"
+                    color={isFavorite ? 'red' : 'black'}
+                    style={{
+                        transition: 'color 1s ',
+                    }}
+                />
+            </button>
+        </div>
+    );
+};
+
+export default UserImage;
