149ddc00a76b5622a13b9d3162c07545ce9bdab3
Artsyom <artem2000z.pinevitch@yandex.by>
2024-02-15 03:52:28 +0300

    deleted dev message

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,15 +2,14 @@ import {Outlet} from 'react-router-dom';
 import Header from './Header/Header';
 import {useEffect} from 'react';
 import {useDispatch} from 'react-redux';
-import {fetchUsers} from '../store/usersSlice/usersSlice';
+import {fetchUsers, loadFavorite} from '../store/usersSlice/usersSlice';
 
 const HomePage = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchUsers());
-        //to do. Delete this message before deploy
-        console.log('DATA FETCHED PLEASE WATCH OUT HOW MANY API CALLS YOU MAKE (This message is for me) ');
+        dispatch(loadFavorite());
     }, [dispatch]);
 
     return (
