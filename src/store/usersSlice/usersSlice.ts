4ce40582a7bcbbc75f30d32df145455b3478b5a1
Artsyom <artem2000z.pinevitch@yandex.by>
2024-02-15 03:55:39 +0300

    added local storage save for liked users

--- src/store/usersSlice/usersSlice.ts
+++ src/store/usersSlice/usersSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice, current} from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async (_, thunkAPI) => {
@@ -12,13 +12,42 @@ export const fetchUsers = createAsyncThunk('users/fetchUsers', async (_, thunkAP
     }
 });
 
+export const fetchUserById = createAsyncThunk('users/fetchUserById', async (id: number, thunkAPI) => {
+    try {
+        const response = await axios.get(`https://dummyjson.com/users/${id}`);
+        console.log('res', response.data);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        return thunkAPI.rejectWithValue(error);
+    }
+});
+
+export const loadFavorite = createAsyncThunk('users/loadFavorite', async () => {
+    const data = localStorage.getItem('isFavorite');
+
+    if (data) {
+        return JSON.parse(data);
+    }
+
+    return {};
+});
+
 export const userSlice = createSlice({
     name: 'users',
     initialState: {
         userList: [],
+        isFavorite: {},
+        singleUserData: [],
         loading: false,
     },
-    reducers: {},
+    reducers: {
+        toggleFavorite: (state, action) => {
+            const id = action.payload;
+            state.isFavorite = {...state.isFavorite, [id]: !state.isFavorite[id]};
+            localStorage.setItem('isFavorite', JSON.stringify(state.isFavorite));
+        },
+    },
     extraReducers: (builder) => {
         builder.addCase(fetchUsers.pending, (state) => {
             state.loading = true;
@@ -27,7 +56,20 @@ export const userSlice = createSlice({
             state.userList = action.payload.users;
             state.loading = false;
         });
+        builder.addCase(fetchUserById.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(fetchUserById.fulfilled, (state, action) => {
+            state.singleUserData = action.payload;
+            state.loading = false;
+        });
+        builder.addCase(loadFavorite.fulfilled, (state, action) => {
+            state.isFavorite = action.payload;
+            state.loading = false;
+        });
     },
 });
+
+export const {toggleFavorite} = userSlice.actions;
 
 export default userSlice.reducer;
