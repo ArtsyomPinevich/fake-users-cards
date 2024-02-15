8e01bd62cf69875a1ca0524e386b14565220d1df
Artsyom <artem2000z.pinevitch@yandex.by>
2024-02-15 03:55:10 +0300

    user layout fixes

--- src/components/UsersLayout/UsersLayout.tsx
+++ src/components/UsersLayout/UsersLayout.tsx
@@ -1,34 +1,56 @@
-import {useState} from 'react';
+import {memo, useEffect, useState} from 'react';
 import {useSelector} from 'react-redux';
 import UserCard from '../UserCard/UserCard';
 import './userLayout.css';
 
 const UsersLayout = () => {
     const {userList, loading} = useSelector((state) => state.users);
-    const [userLimit, setUserLimit] = useState(10);
+    const [userLimit, setUserLimit] = useState(20);
 
-    const limitedUsers = userList.filter((_, i) => i < userLimit);
+    const [filterFavorite, setFilterFavorite] = useState(false);
+
+    const isFavorite = useSelector((state) => state.users.isFavorite);
+    const limitedUsers = userList
+        .filter((user) => {
+            if (filterFavorite) {
+                return isFavorite[user.id];
+            } else {
+                return user;
+            }
+        })
+        .filter((_: unknown, i: number) => i < userLimit);
+
     return (
         <section className="user-cards-section">
-            <div className="cards-layout">
+            <button
+                className="user-card-section-button center-alignment"
+                onClick={() => {
+                    setFilterFavorite(!filterFavorite);
+                }}
+            >
+                {filterFavorite ? 'show all users' : 'show only favorite users'}
+            </button>
+            <div className="cards-layout" style={{transition: '1s all'}}>
                 {loading ? (
                     <p>loading</p>
                 ) : (
                     limitedUsers.map((user) => (
-                        <div className="user-card">
+                        <div className="user-card" key={user.id}>
                             <UserCard user={user} />
                         </div>
                     ))
                 )}
-
-                {userList.length > limitedUsers.length ? (
-                    <button onClick={() => setUserLimit(userLimit + 5)}>+5 users</button>
-                ) : (
-                    <p>there is no more users to show</p>
-                )}
             </div>
+            {userList.length > userLimit ? (
+                //bugged if filtering favorite, need fix
+                <button className="user-card-section-button center-alignment" onClick={() => setUserLimit(userLimit + 5)}>
+                    +5 users
+                </button>
+            ) : (
+                <p className="user-card__no-user-text">there is no more users to show</p>
+            )}
         </section>
     );
 };
 
-export default UsersLayout;
+export default memo(UsersLayout);
